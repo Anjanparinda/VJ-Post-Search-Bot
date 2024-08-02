ede779eb53d3ed823c87f3f456cc397b1c836c4d
Anjanparinda <bphile340@gmail.com>
2024-08-02 11:13:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "")
-DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
-CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
+API_ID       = int(environ.get("API_ID", "23303247"))
+API_HASH     = environ.get("API_HASH", "23623f737dc15708708c65a7297e1510")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "7300349510:AAH9djPuiZCyuj9DqNV-K7U7YadQ_qIC9Hg")
+DATABASE_URI = environ.get("DATABASE_URI", mongodb+srv://Sarkar123:GAUTAMMISHRA@sarkar.1uiwqkd.mongodb.net/?retryWrites=true&w=majority")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002166457568"))
+ADMIN        = int(environ.get("ADMIN", "6931140424"))
+CHANNEL      = environ.get("CHANNEL", "@DevelopperRadha")
